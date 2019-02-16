76d572da15b456b57b538c35feb7b74538d73d7d
Oséias P. Silva <hoseaps@gmail.com>
2019-02-16 18:47:29 -0200

    minificação e concatenação arquivos css e js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,53 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    /* Copia os arquivos para o diretório 'dist' */
     copy: {
       public: {
+        expand: true,
         cwd: "public",
         src: "**",
-        dest: "dist",
-        expand: true
+        dest: "dist"
       }
     },
 
+    /* exclui a pasta dist */
     clean: {
       dist: {
         src: "dist"
       }
+    },
+
+    /* minifica e concatena os arquivos css e js */
+    useminPrepare: {
+      html: "dist/**/*.html"
+    },
+
+    /* aponta para os novos arquivos concatenados e minificados */
+    usemin: {
+      html: "dist/**/*.html"
     }
   });
 
+  //registrando task para exclusao e copia
   grunt.registerTask("dist", ["clean", "copy"]);
-  grunt.registerTask("default", ["dist"]);
 
+  //registrando task para minificação e concatenação
+  grunt.registerTask("minifica", [
+    "useminPrepare",
+    "concat",
+    "uglify",
+    "cssmin",
+    "usemin"
+  ]);
+
+  // registrando default tasks
+  grunt.registerTask("default", ["dist", "minifica"]);
+
+  // carregando tasks
   grunt.loadNpmTasks("grunt-contrib-copy");
   grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-usemin");
 };
